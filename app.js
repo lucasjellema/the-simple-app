4e5eb2774988b02c3d867d7d98b3d0b0fbb9608d
Lucas Jellema <lucas.jellema@amis.nl>
2017-12-02 15:27:11 +0100

    Add support for get request to /stuff

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 
-/**
- * Module dependencies.
- */
 var express = require('express')
-    , http = require('http');
-
-var bodyParser = require('body-parser') // npm install body-parser
+    , http = require('http')
+    , bodyParser = require('body-parser') 
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json()); // for parsing application/json
-
-
-
-
-
-
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
@@ -27,7 +17,6 @@ app.post('/simple-app', function (req, res) {
     handlePost(req, res);
 });
 
-
 app.get('/about', function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write("About Simple App REST API:");
@@ -36,6 +25,52 @@ app.get('/about', function (req, res) {
     res.end();
 });
 
+app.get('/stuff', function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    // from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON
+    var stuff = {
+        "squadName": "Super hero squad",
+        "homeTown": "Metro City",
+        "formed": 2016,
+        "secretBase": "Super tower",
+        "active": true,
+        "members": [
+          {
+            "name": "Molecule Man",
+            "age": 29,
+            "secretIdentity": "Dan Jukes",
+            "powers": [
+              "Radiation resistance",
+              "Turning tiny",
+              "Radiation blast"
+            ]
+          },
+          {
+            "name": "Madame Uppercut",
+            "age": 39,
+            "secretIdentity": "Jane Wilson",
+            "powers": [
+              "Million tonne punch",
+              "Damage resistance",
+              "Superhuman reflexes"
+            ]
+          },
+          {
+            "name": "Eternal Flame",
+            "age": 1000000,
+            "secretIdentity": "Unknown",
+            "powers": [
+              "Immortality",
+              "Heat Immunity",
+              "Inferno",
+              "Teleportation",
+              "Interdimensional travel"
+            ]
+          }
+        ]
+      }
+    res.end(JSON.stringify(stuff));
+});
 
 handlePost =
     function (req, res) {
@@ -44,5 +79,4 @@ handlePost =
         result.summary = "result from simple-app - changed version";
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify(result));
-
     }
